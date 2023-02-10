32241e1e80fb7e3f0d58ce546914093782b8e611
Lotini B. Victor <122792098+victorlotini@users.noreply.github.com>
2023-02-10 09:19:45 +0300

    Add files via upload

---
+++ largest_num.py
@@ -0,0 +1,11 @@
+# Python program to find the largest
+a= int(input("enter the first number"))
+b= int(input("enter the second number"))
+c= int(input("enter the third number"))
+if (a > b) and (a > c):
+        print("wow:")
+elif (b > a) and (b > c):
+        print("great:")
+else:
+        print("great:")
+
